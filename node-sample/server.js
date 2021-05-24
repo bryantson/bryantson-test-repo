8918efae8a0e7ce610675b11859c48f7aef37cd1
Bryant Jimin Son <5396174+bryantson@users.noreply.github.com>
2021-05-24 08:37:10 -0500

    Create server.js

---
+++ node-sample/server.js
@@ -0,0 +1,12 @@
+const express = express("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+server.listen(8080, function(){
+    console.log("Https listening on 8080");
+});
